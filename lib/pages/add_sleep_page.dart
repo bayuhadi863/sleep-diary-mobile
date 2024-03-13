577e0fc6787729186fca08dee190c18f1ea94ff7
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-03-13 15:38:30 +0700

    Implement navigation bar

---
+++ lib/pages/add_sleep_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddSleepPage extends StatefulWidget {
+  const AddSleepPage({super.key});
+
+  @override
+  State<AddSleepPage> createState() => _AddSleepPageState();
+}
+
+class _AddSleepPageState extends State<AddSleepPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Add sleep page'),
+    );
+  }
+}
