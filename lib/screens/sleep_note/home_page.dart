5a36d0114009ade85ecd24d10415f6dc46873266
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-03-18 15:51:36 +0700

    update file structure

--- lib/screens/sleep_note/home_page.dart
+++ lib/screens/sleep_note/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:feather_icons/feather_icons.dart';
+import 'package:sleep_diary_mobile/screens/profile/profile.dart';
+import 'package:sleep_diary_mobile/repositories/authentication/authentication_repository.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,15 +20,32 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Future<void> _selectDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: today,
+      firstDate: DateTime(2010),
+      lastDate: DateTime(2030),
+      cancelText: 'Cancel',
+      confirmText: 'OK',
+      helpText: 'Select date',
+    );
+    if (picked != null && picked != today) {
+      setState(() {
+        today = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromRGBO(224, 238, 225, 1),
-        body: SafeArea(
+        body: SingleChildScrollView(
             child: Column(
           children: [
             const SizedBox(
-              height: 22,
+              height: 42,
             ),
             _greetings(),
             const SizedBox(
@@ -56,15 +76,26 @@ class _HomePageState extends State<HomePage> {
               locale: "en_US",
               rowHeight: 43,
               headerStyle: const HeaderStyle(
-                  formatButtonVisible: false, titleCentered: true),
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
               availableGestures: AvailableGestures.all,
               selectedDayPredicate: (day) => isSameDay(day, today),
               focusedDay: today,
               firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(20230, 3, 14),
+              lastDay: DateTime.utc(2030, 3, 14),
               onDaySelected: _onDaySelected,
             ),
-          )
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          MaterialButton(
+            onPressed: _selectDate,
+            color: Colors.blue,
+            textColor: Colors.white,
+            child: const Text('Milih manual bos'),
+          ),
         ],
       ),
     );
@@ -97,7 +128,7 @@ class _HomePageState extends State<HomePage> {
 
   AspectRatio _card() {
     return AspectRatio(
-      aspectRatio: 336 / 70,
+      aspectRatio: 336 / 110,
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 20),
         decoration: BoxDecoration(
@@ -108,12 +139,13 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
                 alignment: Alignment.topLeft,
                 child: const Text(
                   "Jumat 15-03-2024",
                   style: TextStyle(fontSize: 16),
-                )),       
+                )),
           ],
         ),
       ),
@@ -128,8 +160,62 @@ class _HomePageState extends State<HomePage> {
         children: [
           const Text('Hello, Argya!',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800)),
-          IconButton(onPressed: () {}, icon: const Icon(FeatherIcons.user)),
+          IconButton(
+            onPressed: () {},
+            icon: PopUpMenu(
+              menuList: [
+                PopupMenuItem(
+                  child: ListTile(
+                    leading: Icon(
+                      CupertinoIcons.person,
+                    ),
+                    title: Text("My Profile"),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ProfilePage()),
+                    );
+                  },
+                ),
+                PopupMenuDivider(),
+                PopupMenuItem(
+                  child: ListTile(
+                    leading: Icon(Icons.logout),
+                    title: TextButton(
+                      onPressed: () =>
+                          AuthenticationRepository.instance.logout(),
+                      child: const Text(
+                        'Logout',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class PopUpMenu extends StatelessWidget {
+  final List<PopupMenuEntry<dynamic>> menuList;
+  final Widget? icon;
+  const PopUpMenu({Key? key, required this.menuList, this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      itemBuilder: ((context) => menuList),
+      icon: Icon(
+        CupertinoIcons.person,
       ),
     );
   }
