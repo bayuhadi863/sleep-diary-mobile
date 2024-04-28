6bd9b697ff3055eb96a97240ea0e59cee01cbc03
fiezaeza <fiezanm0404@gmail.com>
2024-04-28 19:45:39 +0700

    reminder

---
+++ lib/screens/sleep_note/monthly_statistic.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MonthlyStatistic extends StatefulWidget {
+  const MonthlyStatistic({super.key});
+
+  @override
+  State<MonthlyStatistic> createState() => _MonthlyStatisticState();
+}
+
+class _MonthlyStatisticState extends State<MonthlyStatistic> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
