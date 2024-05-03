d733f55978540e12c68f27030a3d30e96b688251
argyadwi <argyawoles@gmail.com>
2024-05-03 19:48:56 +0700

    radialBar chart

--- lib/screens/sleep_note/statistik.dart
+++ lib/screens/sleep_note/statistik.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sleep_diary_mobile/controllers/sleep_diary_summary/last_week_summary.dart';
 import 'package:sleep_diary_mobile/controllers/sleep_diary_summary/monthly_summary.dart';
+import 'package:sleep_diary_mobile/screens/sleep_note/summary.dart';
 
 class StatistikPage extends StatefulWidget {
   const StatistikPage({super.key});
@@ -10,10 +12,9 @@ class StatistikPage extends StatefulWidget {
 }
 
 class _StatistikPageState extends State<StatistikPage> {
-  
   // Hanya untuk uji coba backend, boleh dihapus kalau mau dihapus
   @override
-  void initState(){
+  void initState() {
     super.initState();
     testingSummary();
   }
@@ -29,17 +30,55 @@ class _StatistikPageState extends State<StatistikPage> {
     await MonthlySummary().getMonthlyScale(2, 2024);
     await MonthlySummary().getMonthlyFactors(2, 2024);
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(8, 10, 35, 1),
-      body: Center(
-        child: Text(
-          'Halaman Statistik User',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          const SizedBox(
+            height: 700,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 22),
+            height: 80,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: const Color.fromRGBO(38, 38, 66, 1),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Kesimpulan Mingguan',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  GestureDetector(
+                    child: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                    ),
+                    onTap: () {
+                      print('ditekan');
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SummaryPage()));
+                    },
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
